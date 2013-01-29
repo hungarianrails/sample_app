b5a862aca85af65fcdca5088b99321721f7891ec
Nitin Punjabi <nptoronto@yahoo.com>
2013-01-29 13:54:20 +0100

    Find by remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation,
+                  :remember_token
   has_secure_password
 
   before_save { |user| user.email = user.email.downcase }
